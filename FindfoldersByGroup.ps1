f2342cd0bbc21efe9fbf40538fb27b272fb86c65
1ROGUE1 <139764696+1ROGUE1@users.noreply.github.com>
2023-12-06 16:59:20 +0300

    Find folders with needfull group

--- FindfoldersByGroup.ps1
+++ FindfoldersByGroup.ps1
@@ -1,5 +1,6 @@
 ﻿$FolderPath = Get-ChildItem -Directory -Path "\path" -Recurse -Force -Depth 0
 $Output = @() 
+
 ForEach ($Folder in $FolderPath) { 
     $Acl = Get-Acl -Path $Folder.FullName 
     ForEach ($Access in $Acl.Access) { 
@@ -7,4 +8,5 @@ $Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.Ide
 $Output += New-Object -TypeName PSObject -Property $Properties 
     } 
 } 
+
 $Output | Out-GridView
