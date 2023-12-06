8297d92a1cf434d605dd2d3e0c8cebfb5f9ae282
Vlad Muranov <vladikribkin@yandex.ru>
2023-12-06 16:52:30 +0300

    add users from special OU to group

---
+++ Add-Users-to-group-AD.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -Filter {(userAccountControl -ne 514)} -SearchBase "ObjectPath"|ForEach-Object -process {Add-ADGroupMember -identity "GroupName" -Members $_.SamAccountName}
+
+#-Filter {(userAccountControl -ne 514) - атрибут состояния учетной записи, 514 - заблокированная учетка, ne(not equal) - незаблокированная
