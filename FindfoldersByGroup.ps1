1632989926fd40e0803a415af9fe9fa943a2aaa1
Vlad Muranov <vladikribkin@yandex.ru>
2023-12-06 15:23:39 +0300

    :wq

---
+++ FindfoldersByGroup.ps1
@@ -0,0 +1,10 @@
+﻿$FolderPath = Get-ChildItem -Directory -Path "\path" -Recurse -Force -Depth 0
+$Output = @() 
+ForEach ($Folder in $FolderPath) { 
+    $Acl = Get-Acl -Path $Folder.FullName 
+    ForEach ($Access in $Acl.Access) { 
+$Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited} 
+$Output += New-Object -TypeName PSObject -Property $Properties 
+    } 
+} 
+$Output | Out-GridView
